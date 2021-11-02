aa9791980561ae342ffe8751b6de4e110f9f9708
Stephan Wong <80276544+skwong-code@users.noreply.github.com>
2021-11-02 15:24:27 -0700

    routing calls
    routing calls

--- restaurant-app/pages/restaraunts/index.js
+++ restaurant-app/pages/restaraunts/index.js
@@ -1,10 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
+import Link from "next/Link"
 import styles from '../../styles/Home.module.css'
 
 export default function Home() {
+  const restaurants = [
+    {name:"WoodsHill"},
+    {name:"Fiorellas"},
+    {name:"Karma"}
+
+  ]
+
+
   return (
- <h1>Top Restraunt</h1>
-  )
+    <div> 
+ <h1>Restraunt List</h1>
+{restaurants.map( item =>{
+  return <div> 
+
+<Link as={"/restaraunts/"+item.name} href="restaraunts/[restaraunt]">
+<a> {item.name} </a>
+  </Link> 
+ </div>
+})}
+</div>
+ ) 
 }
